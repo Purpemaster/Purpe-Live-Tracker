5b1ebe789ae2dff271d63cf742afb6737fbc8819
Purpemaster <lukas.gastl@icloud.com>
2025-04-24 13:02:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const walletAddress = "9uo3TB4a8synap9VMNpby6nzmnMs9xJWmgo2YKJHZWVn";
+ const walletAddress = "9uo3TB4a8synap9VMNpby6nzmnMs9xJWmgo2YKJHZWVn";
 const heliusApiKey = "2e046356-0f0c-4880-93cc-6d5467e81c73";
+const birdeyeApiKey = "f80a250b67bc411dadbadadd6ecd2cf2";
 const goalUSD = 20000;
 
 const PURPE_MINT = "HBoNJ5v8g71s2boRivrHnfSB5MVPLDHHyVjruPfhGkvL";
@@ -24,15 +25,13 @@ async function fetchSolPrice() {
 
 async function fetchPurpePrice() {
   try {
-    const res = await fetch(
-      `https://quote-api.jup.ag/v6/quote?inputMint=${PURPE_MINT}&outputMint=Es9vMFrzaCERzVw1e8Jdi9bQ5Z3PvAPbpt4nTTbFzmiM&amount=1000000&slippage=1`
-    );
+    const res = await fetch(`https://public-api.birdeye.so/public/price?address=${PURPE_MINT}`, {
+      headers: { "X-API-KEY": birdeyeApiKey }
+    });
     const data = await res.json();
-    const outAmount = parseFloat(data?.outAmount || "0");
-    const price = outAmount / 1_000_000;
-    return price > 0 ? price : fixedPrices[PURPE_MINT];
-  } catch (err) {
-    console.warn("Jupiter QUOTE API-Fehler für PURPE:", err);
+    const value = parseFloat(data?.data?.value || 0);
+    return value > 0 ? value : fixedPrices[PURPE_MINT];
+  } catch {
     return fixedPrices[PURPE_MINT];
   }
 }
